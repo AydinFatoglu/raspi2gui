d0b495d0d0d2fd518014d0ecb6b59b1f807fb3fb
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2023-09-15 18:46:08 +0300

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -7,7 +7,9 @@ sudo raspi-config nonint do_boot_behaviour B4 &&
 sudo apt install chromium -y && 
 sudo apt show mono-complete -y && 
 sudo raspi-config nonint do_boot_wait 0 && 
-sudo raspi-config nonint do_vnc 0
+sudo raspi-config nonint do_vnc 0 && 
+sudo raspi-config nonint do_vnc 1 && 
+sudo raspi-config nonint do_vnc 0 && 
 
 
 
