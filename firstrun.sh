81ccac9997540ed7978612b33b9083b86c78c232
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2023-09-15 18:00:32 +0300

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -8,3 +8,21 @@ sudo apt install chromium -y &&
 sudo apt show mono-complete -y && 
 sudo raspi-config nonint do_boot_wait 0 && 
 sudo raspi-config nonint do_vnc 0
+
+
+# Define the lines to add
+lines_to_add="hdmi_force_hotplug=1
+hdmi_group=2
+hdmi_mode=9"
+
+# Define the config file path
+config_file="/boot/config.txt"
+
+# Check if the config file exists
+if [ -f "$config_file" ]; then
+    # Use sed to insert the lines below #hdmi_safe=1
+    sed -i '/#hdmi_safe=1/a '"$lines_to_add" "$config_file"
+    echo "Lines added to $config_file"
+else
+    echo "Config file $config_file not found."
+fi
