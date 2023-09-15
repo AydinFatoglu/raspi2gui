be1d5bef6fccabbfaef5c837fc0f15cee4a34b13
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2023-09-15 13:45:41 +0300

    Create firstrun.sh

---
+++ firstrun.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update -y && 
+sudo apt-get upgrade -y && 
+sudo apt install lightdm openbox -y && 
+sudo raspi-config nonint do_boot_behaviour B4 && 
+sudo apt install chromium -y && 
+sudo apt show mono-complete -y && 
+sudo raspi-config nonint do_boot_wait 0 && 
+sudo raspi-config nonint do_vnc 0
