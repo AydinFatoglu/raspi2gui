d3cab52433a7ae168fdcde8ef2f780624e4e92b5
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2023-09-16 13:25:00 +0300

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -7,5 +7,8 @@ sudo raspi-config nonint do_boot_behaviour B4 &&
 sudo apt install chromium -y && 
 sudo apt show mono-complete -y && 
 sudo raspi-config nonint do_boot_wait 0 && 
-yes | sudo raspi-config nonint do_vnc 0 && 
+# Run your command and pipe 'Y' to its input
+echo "Y" | sudo raspi-config nonint do_vnc 0 &&
 sudo reboot
+
+
