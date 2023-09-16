8b20b595e5b1ca0493aaafabe54652fbc90beaec
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2023-09-16 19:31:19 +0300

    Create pythonupdate.sh

---
+++ pythonupdate.sh
@@ -0,0 +1,8 @@
+ curl -O https://www.python.org/ftp/python/3.10.13/Python-3.10.13.tgz && 
+ tar -zxvf Python-3.10.13.tgz && 
+ cd Python-3.10.13 && 
+ ./configure --enable-optimizations && 
+ sudo make altinstall && 
+cd /usr/bin && 
+sudo rm python && 
+sudo ln -s /usr/local/bin/python3.10 python
