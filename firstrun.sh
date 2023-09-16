8559f21320dd9f64be5b5bf729a9dc19aa475288
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2023-09-16 17:46:40 +0300

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -2,12 +2,12 @@
 
 sudo apt-get update -y && 
 sudo apt-get upgrade -y && 
-sudo apt install lightdm openbox -y && 
+sudo apt-get install lightdm openbox -y && 
 sudo raspi-config nonint do_boot_behaviour B4 && 
 sudo apt install chromium-browser -y && 
-sudo apt show mono-complete -y && 
+sudo apt-get install mono-complete -y && 
 sudo raspi-config nonint do_boot_wait 0 && 
-sudo apt-get install realvnc-vnc-server -y
+sudo apt-get install realvnc-vnc-server -y && 
 sudo raspi-config nonint do_vnc 0 &&
 curl -O https://raw.githubusercontent.com/AydinFatoglu/raspi2gui/main/cfg_vnc.sh && 
 sudo chmod +x cfg_vnc.sh && 
