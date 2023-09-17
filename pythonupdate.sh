a68dc17c5ea69eef980c15bbf0330cd800c33b1c
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2023-09-17 13:35:43 +0300

    Update pythonupdate.sh

--- pythonupdate.sh
+++ pythonupdate.sh
@@ -1,8 +1,9 @@
- curl -O https://www.python.org/ftp/python/3.10.13/Python-3.10.13.tgz && 
- tar -zxvf Python-3.10.13.tgz && 
- cd Python-3.10.13 && 
- ./configure --enable-optimizations && 
- sudo make altinstall && 
+sudo apt-get remove python3 -y && sudo apt-get remove python2.7 -y && 
+curl -O https://www.python.org/ftp/python/3.10.13/Python-3.10.13.tgz && 
+tar -zxvf Python-3.10.13.tgz && 
+cd Python-3.10.13 && 
+./configure --enable-optimizations && 
+sudo make altinstall && 
 cd /usr/bin && 
 sudo rm python && 
 sudo ln -s /usr/local/bin/python3.10 python
