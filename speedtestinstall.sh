00ed362d410c978171668e9b4bfbfbad455e55d4
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2023-09-19 14:19:13 +0300

    Create speedtestinstall.sh

---
+++ speedtestinstall.sh
@@ -0,0 +1,4 @@
+curl -O https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-armhf.tgz && 
+tar -xzvf ookla-speedtest-1.2.0-linux-armhf.tgz && 
+sudo cp speedtest /usr/local/bin/ && 
+rm -f speedtest speedtest.md speedtest.5 ookla-speedtest-1.2.0-linux-armhf.tgz
